dc08c98c476a23648b6f057061f2d80119dc05a3
Anni Rikkonen <danri008@edu.xamk.fi>
2023-06-29 14:31:17 +0300

    react-router and other base work

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import exerciseMaterialJSON from './exerciseMaterial.json';
+
+function App() {
+
+    let material = exerciseMaterialJSON.material;
+    let multipleChoicesCount = exerciseMaterialJSON.multipleChoices;
+    let opetQuestionsCount = exerciseMaterialJSON.openQuestions; 
+    
+    return (
+        <div className="App">
+            <div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
